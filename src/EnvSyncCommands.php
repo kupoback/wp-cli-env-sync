6a34331838048378c82aa90aed2100b1cd93750f
Nick Makris <nmakris@cliquestudios.com>
2022-01-18 15:30:29 -0500

    Got the dir to zip and compressed with the db file

--- src/EnvSyncCommands.php
+++ src/EnvSyncCommands.php
@@ -11,7 +11,7 @@ class EnvSyncCommands
      * @var string The content directory
      */
     private $contentDir;
-    
+
     /**
      * @var string The uploads directory
      */
@@ -24,14 +24,12 @@ class EnvSyncCommands
 
     public function __construct(array $args = [])
     {
-        $this->uploadsDir = $args['uploads_dir'] ?? false;
-        if (!$this->uploadsDir) {
-            $uploads_dir = wp_get_upload_dir();
-            $this->uploadsDir = $uploads_dir['basedir'] ?? false;
-        }
+
+        $uploads_dir = wp_get_upload_dir();
+        $this->uploadsDir = $uploads_dir['basedir'] ?? false;
+        $this->contentDir = WP_CONTENT_DIR;
 
         $this->tmpDir = $args['tmp_dir'] ?? WP_CLI\Utils\get_temp_dir();
-        $this->contentDir = $args['content_dir'] ?? WP_CONTENT_DIR;
     }
 
     /**
@@ -54,28 +52,47 @@ class EnvSyncCommands
         $site_url = $assoc_args['siteUrl'] ?? false;
 
         if (!class_exists('PharData')) {
-            return WP_CLI::error("This plugin requires PharData to be installed");
+            return WP_CLI::error("This plugin requires PharData to be activated.");
         }
 
         if (!$ip_address) {
-            return WP_CLI::error("You must include the IP address to export files to");
+            return WP_CLI::error("You must include the IP address to export files to.");
         }
 
-        if (!$this->uploadsDir) {
-            return WP_CLI::error('Unable to locate the uploads directory.');
+        if (!$this->tmpDir) {
+            return WP_CLI::error('Unable to set the temporary directory.');
         }
 
-
-
-        WP_CLI::colorize("Starting the export of the database");
+        echo WP_CLI::colorize("Starting the export of the database.\n");
         // Runs the WP-CLI command to export the database
-        // WP_CLI::runcommand("db export mysql.sql");
-        // $cmd = "tar xz --strip-components=1 --directory=%s -f $tarball";
-        // WP_CLI::launch(WP_CLI\Utils\esc_cmd($cmd, $uploads_dir));
+        WP_CLI::runcommand("db export mysql.sql");
+        
+        // Moves the mysql file to the content dir
+        echo WP_CLI::colorize("Moving the database file for compression\n");
+        $db_cmd = "mv mysql.sql %s";
+        WP_CLI::launch(WP_CLI\Utils\esc_cmd($db_cmd, $this->contentDir));
+
+        // Runs the WP-CLI Launch command to go to the content directory and create a zip folder with the uploads directory
+        echo WP_CLI::colorize("Compressing the uploads directory and the mysql.sql file.\n");
+        $compress_cmd = "cd %s && tar -cPzf migrate-dir.tar.gz --exclude=**/cache mysql.sql -C %s ./uploads ";
+        WP_CLI::launch(WP_CLI\Utils\esc_cmd($compress_cmd, $this->contentDir, $this->contentDir, $this->uploadsDir));
+
+        // Delete the mysql file
+        echo WP_CLI::colorize("Deleting the mysql.sql file.\n");
+        $delete_sql_cmd = "rm -f %s/mysql.sql";
+        WP_CLI::launch(WP_CLI\Utils\esc_cmd($delete_sql_cmd, $this->contentDir));
+        
+        // Check if the mysql.sql file still exists, and warn the user.
+        if (file_exists($this->contentDir . "/mysql.sql")) {
+            echo WP_CLI::warning("Unable to delete the mysql.sql file. This is usually due to permissions. Please delete it manually.\n");
+        }
+
+        echo WP_CLI::success("Exported and compressed the uploads folder and mysql.sql file. Preparing to migrate this to the new server.\n");
     }
 
     public function import($args, $assoc_args)
     {
+        // extract tar -xPf /path/to/folder.tar.gz
     }
 
     private function checkForDir($dir, $err)
